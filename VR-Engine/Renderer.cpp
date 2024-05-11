7c386e9d519757f823e22494b6544d482f7e990b
Xavier Knoll <xavierknoll56@gmail.com>
2024-05-10 19:36:25 -0700

    texture struct

--- VR-Engine/Renderer.cpp
+++ VR-Engine/Renderer.cpp
@@ -240,76 +240,14 @@ void Renderer::LoadAssets()
         mConstantBufferAccessors[i].init(m_device.Get(), dhp, &m_constantBufferData, sizeof(SceneConstantBuffer));
     mConstantBufferAccessors[400].init(m_device.Get(), dhp, &m_constantBufferData, sizeof(SceneConstantBuffer));
 
+
     // Note: ComPtr's are CPU objects but this resource needs to stay in scope until
-// the command list that references it has finished executing on the GPU.
-// We will flush the GPU at the end of this method to ensure the resource is not
-// prematurely destroyed.
+      // the command list that references it has finished executing on the GPU.
+      // We will flush the GPU at the end of this method to ensure the resource is not
+      // prematurely destroyed.
+
     ComPtr<ID3D12Resource> textureUploadHeap;
-
-    // Create the texture.
-    {
-        // Describe and create a Texture2D.
-        D3D12_RESOURCE_DESC textureDesc = {};
-        textureDesc.MipLevels = 1;
-        textureDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-        textureDesc.Width = 2;
-        textureDesc.Height = 2;
-        textureDesc.Flags = D3D12_RESOURCE_FLAG_NONE;
-        textureDesc.DepthOrArraySize = 1;
-        textureDesc.SampleDesc.Count = 1;
-        textureDesc.SampleDesc.Quality = 0;
-        textureDesc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
-
-        CD3DX12_HEAP_PROPERTIES heapProps = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT);
-        ThrowIfFailed(m_device->CreateCommittedResource(
-            &heapProps,
-            D3D12_HEAP_FLAG_NONE,
-            &textureDesc,
-            D3D12_RESOURCE_STATE_COPY_DEST,
-            nullptr,
-            IID_PPV_ARGS(&m_texture)));
-
-        const UINT64 uploadBufferSize = GetRequiredIntermediateSize(m_texture.Get(), 0, 1);
-
-        // Create the GPU upload buffer.
-        heapProps = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD);
-        CD3DX12_RESOURCE_DESC resourceDesc = CD3DX12_RESOURCE_DESC::Buffer(uploadBufferSize);
-        ThrowIfFailed(m_device->CreateCommittedResource(
-            &heapProps,
-            D3D12_HEAP_FLAG_NONE,
-            &resourceDesc,
-            D3D12_RESOURCE_STATE_GENERIC_READ,
-            nullptr,
-            IID_PPV_ARGS(&textureUploadHeap)));
-
-        // Copy data to the intermediate upload heap and then schedule a copy 
-        // from the upload heap to the Texture2D.
-        std::vector<UINT8> texture = {
-            255, 255, 255, 255,
-            0,  0, 0, 255,
-            0, 0, 0, 255,
-            255, 255, 255, 255
-        };
-
-        D3D12_SUBRESOURCE_DATA textureData = {};
-        textureData.pData = &texture[0];
-        textureData.RowPitch = 2 * 4;
-        textureData.SlicePitch = textureData.RowPitch * 2;
-
-        UpdateSubresources(m_commandList.Get(), m_texture.Get(), textureUploadHeap.Get(), 0, 0, 1, &textureData);
-        CD3DX12_RESOURCE_BARRIER resourceBarrier  = CD3DX12_RESOURCE_BARRIER::Transition(m_texture.Get(), D3D12_RESOURCE_STATE_COPY_DEST, D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE);
-        m_commandList->ResourceBarrier(1, &resourceBarrier);
-
-        // Describe and create a SRV for the texture.
-        D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc = {};
-        srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
-        srvDesc.Format = textureDesc.Format;
-        srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
-        srvDesc.Texture2D.MipLevels = 1;
-        D3D12_CPU_DESCRIPTOR_HANDLE handle = dhp.m_cbvHeap->GetCPUDescriptorHandleForHeapStart();
-        handle.ptr += m_device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV) * 999;
-        m_device->CreateShaderResourceView(m_texture.Get(), &srvDesc, handle);
-    }
+    texture.init(m_device.Get(), m_commandList.Get(),textureUploadHeap.Get(), dhp);
 
     //create dsv
     // Create the depth stencil view.
@@ -443,8 +381,7 @@ void Renderer::PopulateCommandList()
     //ppHeaps[0] = m_cbvHeap.Get();
     //m_commandList->SetDescriptorHeaps(1, ppHeaps);
     // Bind root descriptor tables
-    m_commandList->SetGraphicsRootDescriptorTable(1, CD3DX12_GPU_DESCRIPTOR_HANDLE(dhp.m_cbvHeap->GetGPUDescriptorHandleForHeapStart(),
-        m_device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV) * 999));
+    texture.bind(m_commandList.Get(), 1);
    // m_commandList->SetGraphicsRootDescriptorTable(1, m_srvHeap->GetGPUDescriptorHandleForHeapStart());
 
 
--- VR-Engine/Renderer.h
+++ VR-Engine/Renderer.h
@@ -50,7 +50,84 @@ using Microsoft::WRL::ComPtr;
 //    }
 //};
 
+struct Texture
+{
+    ID3D12Resource* m_texture;
+    D3D12_GPU_DESCRIPTOR_HANDLE gpuDescriptorHandle;
 
+    void init(ID3D12Device* pDevice, ID3D12GraphicsCommandList* pCommandList, ID3D12Resource* textureUploadHeap, DescriptorHandleProvider& dhp)
+    {
+        // Create the texture.
+        {
+            // Describe and create a Texture2D.
+            D3D12_RESOURCE_DESC textureDesc = {};
+            textureDesc.MipLevels = 1;
+            textureDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+            textureDesc.Width = 2;
+            textureDesc.Height = 2;
+            textureDesc.Flags = D3D12_RESOURCE_FLAG_NONE;
+            textureDesc.DepthOrArraySize = 1;
+            textureDesc.SampleDesc.Count = 1;
+            textureDesc.SampleDesc.Quality = 0;
+            textureDesc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
+
+            CD3DX12_HEAP_PROPERTIES heapProps = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT);
+            ThrowIfFailed(pDevice->CreateCommittedResource(
+                &heapProps,
+                D3D12_HEAP_FLAG_NONE,
+                &textureDesc,
+                D3D12_RESOURCE_STATE_COPY_DEST,
+                nullptr,
+                IID_PPV_ARGS(&m_texture)));
+
+            const UINT64 uploadBufferSize = GetRequiredIntermediateSize(m_texture, 0, 1);
+
+            // Create the GPU upload buffer.
+            heapProps = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD);
+            CD3DX12_RESOURCE_DESC resourceDesc = CD3DX12_RESOURCE_DESC::Buffer(uploadBufferSize);
+            ThrowIfFailed(pDevice->CreateCommittedResource(
+                &heapProps,
+                D3D12_HEAP_FLAG_NONE,
+                &resourceDesc,
+                D3D12_RESOURCE_STATE_GENERIC_READ,
+                nullptr,
+                IID_PPV_ARGS(&textureUploadHeap)));
+
+            // Copy data to the intermediate upload heap and then schedule a copy 
+            // from the upload heap to the Texture2D.
+            std::vector<UINT8> texture = {
+                255, 255, 255, 255,
+                0,  0, 0, 255,
+                0, 0, 0, 255,
+                255, 255, 255, 255
+            };
+
+            D3D12_SUBRESOURCE_DATA textureData = {};
+            textureData.pData = &texture[0];
+            textureData.RowPitch = 2 * 4;
+            textureData.SlicePitch = textureData.RowPitch * 2;
+
+            UpdateSubresources(pCommandList, m_texture, textureUploadHeap, 0, 0, 1, &textureData);
+            CD3DX12_RESOURCE_BARRIER resourceBarrier = CD3DX12_RESOURCE_BARRIER::Transition(m_texture, D3D12_RESOURCE_STATE_COPY_DEST, D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE);
+            pCommandList->ResourceBarrier(1, &resourceBarrier);
+
+            // Describe and create a SRV for the texture.
+            D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc = {};
+            srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+            srvDesc.Format = textureDesc.Format;
+            srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
+            srvDesc.Texture2D.MipLevels = 1;
+            D3D12_CPU_DESCRIPTOR_HANDLE handle = dhp.getCpuHandle(pDevice);
+            pDevice->CreateShaderResourceView(m_texture, &srvDesc, handle);
+            gpuDescriptorHandle = dhp.GpuHandleFromCpuHandle(handle);
+        }
+    }
+
+    void bind(ID3D12GraphicsCommandList* pCommandList, UINT rootParameterIndex)
+    {
+        pCommandList->SetGraphicsRootDescriptorTable(rootParameterIndex, gpuDescriptorHandle);
+    }
+};
 
 class Renderer : public DXSample
 {
@@ -110,7 +187,8 @@ private:
     //ComPtr<ID3D12Resource> m_constantBuffer;
     //UINT8* m_pCbvDataBegin;
     SceneConstantBuffer m_constantBufferData;
-    ComPtr<ID3D12Resource> m_texture;
+    Texture texture;
+    
     ComPtr<ID3D12Resource> m_depthStencil;
     VertexBuffer mCubeVB;
     VertexBuffer mPlaneVB;
